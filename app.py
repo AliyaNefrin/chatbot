bd58f1b4a4c1b282e8c6ea0a8419c785148b377a
AliyaNefrin <aliyanefrin8@gmail.com>
2025-03-02 07:56:44 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+menu={
+    "Biriyani":{"price":250,"available":True},
+    "Tandoori Chicken":{"price":350,"available":True},
+    "Butter Naan":{"price":40,"available":True},
+    "Dragon chicken":{"price":250,"available":False}
+}
+
+@app.route('/webhook',methods=['POST'])
+def webhook():
+    req = request.get_json()
+    dish = req["queryResult"]["parameters"]["dish"]
+    if dish in menu:
+        price = menu[dish]["price"]
+        available = "available" if menu[dish]["available"] else"not available"
+        response = f"The price of {dish} is ₹{price}. It is currently {available}."
+    else:
+        response = f"Sorry, we don't have {dish} on our menu."
+    return jsonify({"fulfillmentText:response"})
+
+if __name__=='__main__':
+    app.run(port=5000)
+
+
+  
