06827590c6994fcabf226fd8755f90a61c752880
AliyaNefrin <aliyanefrin8@gmail.com>
2025-03-02 08:28:13 +0530

    Initial commit

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def webhook():
     return jsonify({"fulfillmentText:response"})
 
 if __name__=='__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
 
 
   
